1f1db00ec1abad8816fd700f7d252075a679802a
Jorgen Nilsson <me@jorgennilsson.com>
2019-04-20 17:37:20 +0200

    deployment

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+# The standard nginx container just runs nginx. The configuration file added
+# below will be used by nginx.
+FROM nginx
+
+# create log dir configured in nginx.conf
+RUN mkdir -p /var/log/app_engine
+
+# Create a simple file to handle heath checks. Health checking can be disabled
+# in app.yaml, but is highly recommended. Google App Engine will send an HTTP
+# request to /_ah/health and any 2xx or 404 response is considered healthy.
+# Because 404 responses are considered healthy, this could actually be left
+# out as nginx will return 404 if the file isn't found. However, it is better
+# to be explicit.
+RUN mkdir -p /var/www/public/_ah && echo "healthy" > /var/www/public/_ah/health
+
+# Copy the nginx configuration file. This sets up the behavior of nginx, most
+# importantly, it ensure nginx listens on port 8080. Google App Engine expects
+# the runtime to respond to HTTP requests at port 8080.
+COPY nginx.conf /etc/nginx/nginx.conf
+
+# Finally, all static assets.
+ADD dist/ /var/www/public/
+RUN chmod -R a+r /var/www/public
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,102 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <form action="/" v-if="!codesValid">
+      <h1>The codes</h1>
+      <span>{{result}}</span>
+      <div class="input-cards">
+        <div v-for="(hunter, index) in hunters" :key="hunter.name+index" class="input-card">
+          <h2>{{hunter.name}}</h2>
+          <input type="text" maxlength="1" v-model="hunters[index].code" v-on:input="allCodes">
+        </div>
+      </div>
+      <div v-if="validatingCodes">
+        <loader/>
+      </div>
+    </form>
+    <div v-if="codesValid">
+      <h1>Välkommna till högkvarteret!</h1>
+
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import loader from '../components/loader'
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    loader
+  },
+  data () {
+    return {
+      hunters: [
+        {
+          name: 'Elis',
+          code: null
+        },
+        {
+          name: 'Elna',
+          code: null
+        },
+        {
+          name: 'Elliot',
+          code: null
+        },
+        {
+          name: 'Cornelia',
+          code: null
+        }
+      ],
+      result: '',
+      codesValid: false,
+      validatingCodes: false
+    }
+  },
+  methods: {
+    allCodes: function () {
+      const codes = this.hunters.map((hunter) => {
+        return hunter.code
+      })
+
+      this.result = codes.join('')
+
+      if (this.result === '1234') {
+        this.validatingCodes = true
+        const timeoutID = window.setTimeout(() => {
+          this.codesValid = true
+          this.validatingCodes = false
+          window.clearTimeout(timeoutID)
+        }, 2000)
+      }
+    }
   }
 }
 </script>
+
+<style lang="scss">
+  input {
+    display: inline-block;
+    border: 1px solid gray;
+    border-radius: 7px;
+    width: 80px;
+    padding: 20px;
+    font-size: 64px;
+    text-align: center;
+  }
+
+  .input-cards {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .input-card {
+
+  }
+
+  svg {
+    width: 100px;
+    height: 100px;
+    margin: 20px;
+    display:inline-block;
+  }
+</style>
